4a47182f676e2ab3f232aaa80015c4c59e720082
admin@extentsoft.com <admin@extentsoft.com>
2017-07-12 13:24:57 +0700

    Tested grunt

--- webapp/Gruntfile.js
+++ webapp/Gruntfile.js
@@ -1,5 +1,5 @@
 module.exports = function(grunt){
-  grunt.initConfig(
+  grunt.initConfig({
     sass: {
       dist: {
         files: {
@@ -7,7 +7,7 @@ module.exports = function(grunt){
         }
       }
     }
-  );
+  });
 
   grunt.loadNpmTasks('grunt-sass');
   grunt.registerTask('default', ['sass']);
